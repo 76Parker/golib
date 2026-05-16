cfa6039b538cedfc8d31c84c2c153e8bd58c163d
PArk <evilsizemoney@mail.ru>
2026-05-16 22:13:24 +0300

    feature(httplib): add initializing *http.Server

---
+++ httplib/server.go
@@ -0,0 +1,33 @@
+package httpserver
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"time"
+)
+
+type Config struct {
+	Address           string        `yaml:"address"`
+	ReadHeaderTimeout time.Duration `yaml:"read_header_timeout"`
+	ReadTimeout       time.Duration `yaml:"read_timeout"`
+	WriteTimeout      time.Duration `yaml:"write_timeout"`
+	IdleTimeout       time.Duration `yaml:"idle_timeout"`
+	MaxHeaderBytes    int           `yaml:"max_header_bytes"`
+}
+
+func NewHTTPServer(ctx context.Context, cfg Config, handler http.Handler) *http.Server {
+	s := &http.Server{
+		Addr:              cfg.Address,
+		Handler:           handler,
+		ReadTimeout:       cfg.ReadTimeout,
+		ReadHeaderTimeout: cfg.ReadHeaderTimeout,
+		WriteTimeout:      cfg.WriteTimeout,
+		IdleTimeout:       cfg.IdleTimeout,
+		MaxHeaderBytes:    cfg.MaxHeaderBytes,
+		BaseContext: func(listener net.Listener) context.Context {
+			return ctx
+		},
+	}
+	return s
+}
